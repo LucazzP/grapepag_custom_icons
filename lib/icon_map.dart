e4d8116896bc6a2a369cddac5d7248cf0d4e19c8
LucazzP <lucashpolazzo2013@gmail.com>
2019-11-08 09:03:49 -0300

    Alterado a forma de capturar icones, ao invez de codigo, pelo nome do icon

---
+++ lib/icon_map.dart
@@ -0,0 +1,12 @@
+Map<String, int> iconMap = {
+  'basket': 0xe800,
+  'arrow_down': 0xe801,
+  'money': 0xe802,
+  'burger': 0xe809,
+  'credit_cart': 0xe80a,
+  'cupcake': 0xe80b,
+  'debit_card': 0xe80c,
+  'french_fries': 0xe80d,
+  'leaf': 0xe80e,
+  'soda': 0xe80f,
+};
