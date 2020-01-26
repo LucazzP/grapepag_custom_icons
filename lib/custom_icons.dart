55c779274ce67ef908e8413b103e3bcb4ade69ca
Gabriel Sávio <gabulsavul@outlook.com>
2020-01-26 21:00:56 +0000

    Updated custom_icons.dart class public

--- lib/custom_icons.dart
+++ lib/custom_icons.dart
@@ -3,38 +3,40 @@ import 'package:flutter/widgets.dart';
 
 class CustomIcons {
   CustomIcons._();
-  
-  static const IconData basket = const _CustomIconData(0xe800, 'basket');
-  static const IconData arrow_down = const _CustomIconData(0xe801, 'arrow_down');
-  static const IconData money = const _CustomIconData(0xe802, 'money');
-  static const IconData burger = const _CustomIconData(0xe809, 'burger');
-  static const IconData credit_cart = const _CustomIconData(0xe80a, 'credit_cart');
-  static const IconData cupcake = const _CustomIconData(0xe80b, 'cupcake');
-  static const IconData debit_card = const _CustomIconData(0xe80c, 'debit_card');
-  static const IconData french_fries = const _CustomIconData(0xe80d, 'french_fries');
-  static const IconData leaf = const _CustomIconData(0xe80e, 'leaf');
-  static const IconData soda = const _CustomIconData(0xe80f, 'soda');
+
+  static const IconData basket = const CustomIconData(0xe800, 'basket');
+  static const IconData arrow_down = const CustomIconData(0xe801, 'arrow_down');
+  static const IconData money = const CustomIconData(0xe802, 'money');
+  static const IconData burger = const CustomIconData(0xe809, 'burger');
+  static const IconData credit_cart =
+      const CustomIconData(0xe80a, 'credit_cart');
+  static const IconData cupcake = const CustomIconData(0xe80b, 'cupcake');
+  static const IconData debit_card = const CustomIconData(0xe80c, 'debit_card');
+  static const IconData french_fries =
+      const CustomIconData(0xe80d, 'french_fries');
+  static const IconData leaf = const CustomIconData(0xe80e, 'leaf');
+  static const IconData soda = const CustomIconData(0xe80f, 'soda');
 
   static IconData fromString(String key) {
     int codePoint = iconMap[key];
-    return _CustomIconData(codePoint, key);
+    return CustomIconData(codePoint, key);
   }
 
-  IconData operator [] (String key) {
+  IconData operator [](String key) {
     int codePoint = iconMap[key];
-    return _CustomIconData(codePoint, key);
+    return CustomIconData(codePoint, key);
   }
 }
 
-class _CustomIconData extends IconData {
+class CustomIconData extends IconData {
   final String name;
 
-  const _CustomIconData(int codePoint, this.name)
+  const CustomIconData(int codePoint, this.name)
       : super(
-    codePoint,
-    fontFamily: 'CustomIcons',
-    fontPackage: 'custom_icons',
-  );
+          codePoint,
+          fontFamily: 'CustomIcons',
+          fontPackage: 'custom_icons',
+        );
 
   String getName() {
     return this.name;
