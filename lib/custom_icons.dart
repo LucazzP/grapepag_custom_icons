9a4ccfa173b4b64b020ce8e30e80ea6e3759c8e6
LucazzP <lucashpolazzo2013@gmail.com>
2020-07-13 20:56:15 -0300

    Corrigido margem dos icones

--- lib/custom_icons.dart
+++ lib/custom_icons.dart
@@ -1,21 +1,24 @@
 import 'package:custom_icons/icon_map.dart';
 import 'package:flutter/widgets.dart';
 
+import 'icon_map.dart';
+
+@immutable
 class CustomIcons {
   CustomIcons._();
 
-  static const IconData basket = const CustomIconData(0xe800, 'basket');
-  static const IconData arrow_down = const CustomIconData(0xe801, 'arrow_down');
-  static const IconData money = const CustomIconData(0xe802, 'money');
+  static const IconData basket = const CustomIconData(0xe808, 'basket');
+  static const IconData arrow_down = const CustomIconData(0xe807, 'arrow_down');
+  static const IconData money = const CustomIconData(0xe805, 'money');
   static const IconData burger = const CustomIconData(0xe809, 'burger');
   static const IconData credit_cart =
-      const CustomIconData(0xe80a, 'credit_cart');
-  static const IconData cupcake = const CustomIconData(0xe80b, 'cupcake');
-  static const IconData debit_card = const CustomIconData(0xe80c, 'debit_card');
+      const CustomIconData(0xe800, 'credit_cart');
+  static const IconData cupcake = const CustomIconData(0xe801, 'cupcake');
+  static const IconData debit_card = const CustomIconData(0xe802, 'debit_card');
   static const IconData french_fries =
-      const CustomIconData(0xe80d, 'french_fries');
-  static const IconData leaf = const CustomIconData(0xe80e, 'leaf');
-  static const IconData soda = const CustomIconData(0xe80f, 'soda');
+      const CustomIconData(0xe803, 'french_fries');
+  static const IconData leaf = const CustomIconData(0xe804, 'leaf');
+  static const IconData soda = const CustomIconData(0xe806, 'soda');
 
   static IconData fromString(String key) {
     int codePoint = iconMap[key];
@@ -28,8 +31,16 @@ class CustomIcons {
   }
 }
 
+@immutable
 class CustomIconData extends IconData {
   final String name;
+
+  CustomIconData.fromName(this.name)
+      : super(
+          iconMap[name],
+          fontFamily: 'CustomIcons',
+          fontPackage: 'custom_icons',
+        );
 
   const CustomIconData(int codePoint, this.name)
       : super(
@@ -37,6 +48,11 @@ class CustomIconData extends IconData {
           fontFamily: 'CustomIcons',
           fontPackage: 'custom_icons',
         );
+
+  @override
+  String toString() {
+    return getName();
+  }
 
   String getName() {
     return this.name;
--- lib/icon_map.dart
+++ lib/icon_map.dart
@@ -1,12 +1,12 @@
-Map<String, int> iconMap = {
-  'basket': 0xe800,
-  'arrow_down': 0xe801,
-  'money': 0xe802,
+const Map<String, int> iconMap = const {
+  'basket': 0xe808,
+  'arrow_down': 0xe807,
+  'money': 0xe805,
   'burger': 0xe809,
-  'credit_cart': 0xe80a,
-  'cupcake': 0xe80b,
-  'debit_card': 0xe80c,
-  'french_fries': 0xe80d,
-  'leaf': 0xe80e,
-  'soda': 0xe80f,
+  'credit_cart': 0xe800,
+  'cupcake': 0xe801,
+  'debit_card': 0xe803,
+  'french_fries': 0xe803,
+  'leaf': 0xe804,
+  'soda': 0xe806,
 };
